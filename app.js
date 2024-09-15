6631b5453770dec9415225620dd0daad2c700baf
Mijryanbitao <mijryanb@gmail.com>
2024-09-15 16:44:56 +0800

    Create app.js

---
+++ app.js
@@ -0,0 +1,20 @@
+//Create a variable named favActorFirstName and store your favorite actor's first name
+let favActorFirstName = "Sanya";
+//Create a variable named favActorLastName and store their last name
+let favActorLastName = "Lopez";
+let fullName = favActorFirstName + "" + favActorLastName;
+
+let uppercase = fullName.toLowerCase();
+
+let lowercase = "Rio".toUpperCase();
+
+let message = "My favorite actor is " + fullName.toUpperCase();
+
+message += ",her best show is Encantadia.";
+
+console.log(favActorFirstName);
+console.log(favActorLastName);
+console.log(fullname);
+console.log(uppercase);
+console.log(lowercase);
+console.log(message);
